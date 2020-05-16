79f7d8cc37c444c0e1409bc40d62d6497ccef425
Andrea <andrea.formichetti@student.univaq.it>
2020-05-16 19:52:15 +0200

    cose

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -6,12 +6,12 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <! — csrf token ->
-        <meta name="csrf-token" content="{{ csrf_token() }}">
-        <title>Your Website Title</title>
 
-        <! — styles ->
-            <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <title>Your Website Title</title>
+
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+
 </head>
 
 <body>
