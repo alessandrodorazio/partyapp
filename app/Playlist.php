a2ef953c35be82a6157e4de82f66c36406a2cbbc
gabriele <gabriele.colapelle@gmail.com>
2020-05-25 11:52:01 +0200

    playlist

--- app/Playlist.php
+++ app/Playlist.php
@@ -2,9 +2,33 @@
 
 namespace App;
 
+use App\User;
 use Illuminate\Database\Eloquent\Model;
 
 class Playlist extends Model
 {
     //
+    protected $table = "playlist";
+    protected $fillable = ["name", "genre_id", "owner_id"];
+    protected $appends = ["genre"];
+
+    public function owner()
+    {
+        return $this->belongsTo(User::class, 'owner_id', 'id');
+    }
+
+    public function genre()
+    {
+        return $this->belongsTo(MusicalGenre::class, 'genre_id', 'id');
+    }
+
+    public function song()
+    {
+        return $this->belongsToMany(Song::class, 'playlist_song', 'playlist_id', 'song_id', 'id', 'id');
+    }
+
+    public function getGenreAttribute()
+    {
+        return $this->genre()->first();
+    }
 }
